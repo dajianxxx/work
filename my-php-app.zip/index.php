8f2dd18cf0c0ef210ae471ce32e6bc1a61af0ef9
dajianxxx <125621808+dajianxxx@users.noreply.github.com>
2025-07-29 19:12:40 +0900

    Update index.php

--- my-php-app.zip/index.php
+++ my-php-app.zip/index.php
@@ -3,10 +3,10 @@
 <head>
     <title>Welcome to Azure PHP App</title>
     <link rel="stylesheet" href="assets/style.css">
-</head>
+<head>
 <body>
     <h1>Hello from Azure PHP App</h1>
     <p>This is the home page served by PHP.</p>
     <a href="about.php">Go to About Page</a>
-</body>
-</html>
+<body>
+<html>
