b38eb709c13aceb7bb738e72c6e2b53ad506ba65
dajianxxx <125621808+dajianxxx@users.noreply.github.com>
2025-07-29 18:51:22 +0900

    Create index.php

---
+++ my-php-app/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Welcome to Azure PHP App</title>
+    <link rel="stylesheet" href="assets/style.css">
+</head>
+<body>
+    <h1>Hello from Azure PHP App</h1>
+    <p>This is the home page served by PHP.</p>
+    <a href="about.php">Go to About Page</a>
+</body>
+</html>
