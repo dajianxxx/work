a033dce7b0b6bf404262a89b03fb1b7c84bf6cf3
dajianxxx <125621808+dajianxxx@users.noreply.github.com>
2025-07-29 18:51:52 +0900

    Create about.php

---
+++ my-php-app/about.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>About Page</title>
+    <link rel="stylesheet" href="assets/style.css">
+</head>
+<body>
+    <h1>About</h1>
+    <p>This page is rendered by PHP too.</p>
+    <a href="index.php">Back to Home</a>
+</body>
+</html>
